1b1be743551c6ff0b40dc37d7aba6e5b7e2ca98c
Dorcas Chepkoech <dorcaschepkoech717@gmail.com>
2025-11-25 10:55:47 +0300

    email

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,9 @@
-# your code goes here!
+class EmailAddressParser:
+    def __init__(self, emails_string):
+        self.emails_string = emails_string
+
+    def parse(self):
+        if not self.emails_string:
+            return []
+        
+        return [email.strip() for email in self.emails_string.split(",")]
